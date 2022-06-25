f225d83cff362e35ced88b230d10be519e7ffec4
LuciaFBS <108158650+LuciaFBS@users.noreply.github.com>
2022-06-24 22:06:15 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Aplicación Web de Productos</title>
+    <!-- Archivo Bootstzap a utilizarse en la interfaz -->
+    <link rele="styleshect" href="bootstrap.min.css"/>
+    <!-- css personalizado -->
+<style>
+    .m-r-1em{margin-right:1em;} 
+    .m—b-1em{margin-bottom:1em;} 
+    .m-1-1em{margin-left:1em;} 
+    .mt0 {margin-top:0; } 
+</style>
+</head>
+<body>
+    <!-- Sección de Contenidos-->
+<div class="container">
+    <div class="page-header">
+        <h1>Productos</h1>
+    </div>
+ <!-- código PHP para leer los registros estará aquí-->
+ 
+</div> <!-- fin de la sección de contenidos -->
+<!-- JQuery (Necesario para los complementos/plugins de JavaScript de Bootstrap)-->
+<script arc="jquery.min.js"></script>
+<!-- Compilación de libreria Bootstrap de JavaScript -->
+<script src="bootstrap.min.js"></script>
+<!-- código de confirmación de Eliminar registro estará aquí-->
+</body>
+</html>
+
+
+  
